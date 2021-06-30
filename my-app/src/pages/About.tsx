3d5cb4f5495f2f6000254721bed50858de402d12
Gunnar <Gunnar.Kleiven@student.uib.no>
2021-06-30 15:42:28 +0200

    (Hopefully) responsive font sizes over different screen sizes

--- my-app/src/pages/About.tsx
+++ my-app/src/pages/About.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import { makeStyles, createStyles, Theme, createMuiTheme, responsiveFontSizes, ThemeProvider } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Card from '@material-ui/core/Card';
@@ -21,10 +21,12 @@ const useStyles = makeStyles((theme: Theme) =>
             padding: theme.spacing(1),
         },
         media: {
-
         },
     }),
 );
+
+let theme = createMuiTheme();
+theme = responsiveFontSizes(theme);
 
 const About: React.FC<AboutProps> = () => {
     const classes = useStyles();
@@ -41,32 +43,34 @@ const About: React.FC<AboutProps> = () => {
                     spacing={3}
                 >
                     <Grid item xs={10}>
-                        <Typography variant="h6" align="center" paragraph>
-                            <br />
-                            Om oss
-                        </Typography>
-                        <Typography variant="body1" align="center">
+                        <ThemeProvider theme={theme}>
+                            <Typography variant="h6" align="center" paragraph>
+                                <br />
+                                Om oss
+                            </Typography>
+                            <Typography variant="body1" align="center">
 
-                            Me er eit selskap med base i Sogndal som driv med bergboring. Me starta våren 2014 og er no 4 ansatte. Arbeidsområdet er Sogn og Fjordane, Hordaland og Møre og Romsdal, men me er fleksible utover dette.
+                                Me er eit selskap med base i Sogndal som driv med bergboring. Me starta våren 2014 og er no 4 ansatte. Arbeidsområdet er Sogn og Fjordane, Hordaland og Møre og Romsdal, men me er fleksible utover dette.
 
-                            <br />
-                            Eigar er Per Magne Fimreite.
-                            <br />
-                            <br />
-                        </Typography>
-                        <Typography variant="h6" align="center">
-                            Me tilbyr tenester til konkurransedyktige prisar innan:
-                        </Typography>
-                        <Typography variant="body1" paragraph>
-                            <br />
-                            - Boring etter vatn for vassforsyning til gardsbruk, bustadhus, hytter, bustadfelt osv.
-                            <br />
-                            - Boring av energibrønnar til varmepumper i bustadhus, dette er ei eingongsinvistering som gjer at du kan utnytte ein miljøvenleg og fornybar energiresurs.
-                            <br />
-                            - Gjennomboring av fjellknausar, kryssing av vegar, tomter og eksisterande bebyggelse. Dette er eit rimleg og miljøvenleg alternativ til konvensjonelle grøfter.
-                            <br />
-                            -Fundamentering: Er ein teknikk som blir benytta der det er usikker grunn for etablering av bygg.
-                        </Typography>
+                                <br />
+                                Eigar er Per Magne Fimreite.
+                                <br />
+                                <br />
+                            </Typography>
+                            <Typography variant="h6" align="center">
+                                Me tilbyr tenester til konkurransedyktige prisar innan:
+                            </Typography>
+                            <Typography variant="body1" paragraph>
+                                <br />
+                                - Boring etter vatn for vassforsyning til gardsbruk, bustadhus, hytter, bustadfelt osv.
+                                <br />
+                                - Boring av energibrønnar til varmepumper i bustadhus, dette er ei eingongsinvistering som gjer at du kan utnytte ein miljøvenleg og fornybar energiresurs.
+                                <br />
+                                - Gjennomboring av fjellknausar, kryssing av vegar, tomter og eksisterande bebyggelse. Dette er eit rimleg og miljøvenleg alternativ til konvensjonelle grøfter.
+                                <br />
+                                -Fundamentering: Er ein teknikk som blir benytta der det er usikker grunn for etablering av bygg.
+                            </Typography>
+                        </ThemeProvider>
                     </Grid>
                     <Grid item xs={12}>
                         <Card>
