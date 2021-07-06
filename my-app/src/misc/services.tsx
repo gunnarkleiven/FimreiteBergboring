0c86b9be6054924b8f5c278bec86ae5026136723
Gunnar <Gunnar.Kleiven@student.uib.no>
2021-07-06 21:15:22 +0200

    Upgraded the menu drawer, it is now a proper list, and it expands on the services tab

---
+++ my-app/src/misc/services.tsx
@@ -0,0 +1,22 @@
+import { NameAndPath } from "../App";
+
+const services: NameAndPath[] = [
+    {
+        name: "Boring etter vatn",
+        path: "/water/",
+    },
+    {
+        name: "Gjennomboring",
+        path: "/piercing/",
+    },
+    {
+        name: "Energibrønnar",
+        path: "/energywells/",
+    },
+    {
+        name: "Fundamentering",
+        path: "/fundament/",
+    },
+]
+
+export default services;
